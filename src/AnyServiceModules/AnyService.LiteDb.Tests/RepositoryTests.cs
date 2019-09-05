44dbce51e86e7c570810b3494f77c54e4b67f3d6
Saturn Technologies <info@saturn72.com>
2019-09-05 16:20:00 +0300

    add deleted code

---
+++ src/AnyServiceModules/AnyService.LiteDb.Tests/RepositoryTests.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+
+namespace AnyService.LiteDb.Tests
+{
+    public class RepositoryTests
+    {
+        public class TestDomainModel : IDomainModelBase
+        {
+            public string Id { get; set; }
+            public string Value { get; set; }
+        }
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private string GetCurrentMethodName()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);
+
+            return sf.GetMethod().Name;
+        }
+
+        [Fact]
+        public async Task Insert()
+        {
+            var dbName = $"testdb-{GetCurrentMethodName()}-{DateTime.UtcNow.ToString("yyyy-mm-dd_hh-mm-dd-fff")}.db";
+
+            var lr = new AnyService.LiteDb.Repository<TestDomainModel>(dbName);
+            var expValue = "my special value";
+            var data = new TestDomainModel
+            {
+                Value = expValue
+            };
+
+            var dbRes = (await lr.Insert(data)) as TestDomainModel;
+            dbRes.Id.ShouldNotBeNullOrEmpty();
+            dbRes.Value.ShouldBe(expValue);
+            dbRes.ShouldBe(data);
+        }
+    }
+}
