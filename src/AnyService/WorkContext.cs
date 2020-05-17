cb8ddbed481a7f5d749715a31f38e03fc1f6cc03
roi <shabtai>
2020-05-17 12:01:57 +0300

    add parameters to workcontext

--- src/AnyService/WorkContext.cs
+++ src/AnyService/WorkContext.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace AnyService
 {
-    public sealed class WorkContext
+    public class WorkContext
     {
         public Type CurrentType => CurrentEntityConfigRecord?.Type;
         public EntityConfigRecord CurrentEntityConfigRecord { get; set; }
         public string CurrentUserId { get; set; }
         public RequestInfo RequestInfo { get; set; }
+
+        /// <summary>
+        /// Placeholder for all workcontext items that do nt have dedicated property
+        /// </summary>
+        /// <value></value>
+        public IReadOnlyDictionary<string, object> Parameters { get; set; }
     }
 }
