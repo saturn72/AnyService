35559da52783a90f4e395cb031353d88032f6041
roi <shabtai>
2020-05-24 15:03:06 +0300

    add WorkContextExtensions

---
+++ src/AnyService.Tests/WorkContextExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AnyService.Tests
+{
+    public class WorkContextExtensionsTests
+    {
+        [Fact]
+        public void GetParameterByIndex_NullParameters_Throws()
+        {
+            var wc = new WorkContext
+            {
+            };
+            Should.Throw<NullReferenceException>(() => wc.GetParameterByIndex<string>("some-key", 1));
+        }
+        [Fact]
+        public void GetParameterByIndex_NullParameters_ThrowsOnOutOfRange()
+        {
+            var key = "k";
+            var wc = new WorkContext
+            {
+                Parameters = new Dictionary<string, object>
+                {
+                    {key, new[]{"eee" } }
+                }
+            };
+            Should.Throw<ArgumentOutOfRangeException>(() => wc.GetParameterByIndex<string>(key, 1));
+        }
+        [Fact]
+        public void GetParameterByIndex_KeyNotExists_returnsDefault()
+        {
+            var wc = new WorkContext
+            {
+                Parameters = new Dictionary<string, object> { }
+            };
+            wc.GetParameterByIndex<string>("not-exists", 1).ShouldBe(default);
+        }
+        [Fact]
+        public void GetParameterByIndex_ReturnsValue()
+        {
+            string key = "k", value = "v";
+            var wc = new WorkContext
+            {
+                Parameters = new Dictionary<string, object>
+                {
+                    { key, new[]{"ssss", value, "rr", "wer" } }
+                }
+            };
+            wc.GetParameterByIndex<string>(key, 1).ShouldBe(value);
+        }
+
+        [Fact]
+        public void GetFirstParameterByIndex_ReturnsValue()
+        {
+            string key = "k", value = "v";
+            var wc = new WorkContext
+            {
+                Parameters = new Dictionary<string, object>
+                {
+                    { key, new[]{value, "rr", "wer" } }
+                }
+            };
+            wc.GetFirstParameter<string>(key).ShouldBe(value);
+        }
+    }
+}
---
+++ src/AnyService/WorkContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AnyService
+{
+    public static class WorkContextExtensions
+    {
+        public static T GetFirstParameter<T>(this WorkContext workContext, string parameterName)
+        {
+            return GetParameterByIndex<T>(workContext, parameterName, 0);
+        }
+        public static T GetParameterByIndex<T>(this WorkContext workContext, string parameterName, int index)
+        {
+            if (!workContext.Parameters.TryGetValue(parameterName, out object value))
+                return default;
+            return (value as IEnumerable<T>).ElementAt(index);
+        }
+    }
+}
