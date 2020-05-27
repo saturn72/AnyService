2c8ce0de77a6d2f4431c28896af1ec0faf01b5f8
roi <shabtai>
2020-05-27 14:50:04 +0300

    add json extension method
    publish to nuget

--- src/AnyService.Utilities.Tests/Extensions/JsonExtensionsTests.cs
+++ src/AnyService.Utilities.Tests/Extensions/JsonExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Shouldly;
 using System;
+using System.Reflection.Metadata;
 using System.Text.Json;
 using Xunit;
 
@@ -24,6 +25,65 @@ namespace AnyService.Utilities.Tests.Extensions
             var o2 = t.ShouldBeOfType<TestClass>();
             o2.Name.ShouldBe(Name);
             o2.Value.ShouldBe(Value);
+        }
+        [Fact]
+        public void GetValue_SpecificTypes()
+        {
+            var expDateTime = DateTime.UtcNow;
+            var expDateTimeOffset = DateTimeOffset.UtcNow;
+            var expGuid = new Guid("72e791db-5223-4e06-9caf-39fd4b593de4");
+            var o = new
+            {
+                DateTime = expDateTime,
+                DateTimeOffset = expDateTimeOffset,
+                Guid = "72e791db-5223-4e06-9caf-39fd4b593de4",
+            };
+
+            var jsonstring = JsonSerializer.Serialize(o);
+            using var jDoc = JsonDocument.Parse(jsonstring);
+            var root = jDoc.RootElement;
+            root.GetValue(expDateTime.GetType(), "DateTime").ShouldBe(expDateTime);
+            root.GetValue(expDateTimeOffset.GetType(), "DateTimeOffset").ShouldBe(expDateTimeOffset);
+            root.GetValue(expGuid.GetType(), "Guid").ShouldBe(expGuid);
+        }
+
+        [Theory]
+        [InlineData(typeof(bool), "bo", true)]
+        [InlineData(typeof(byte), "by", 1)]
+        [InlineData(typeof(decimal), "Decimal", 123)]
+        [InlineData(typeof(double), "Double", 123)]
+        [InlineData(typeof(short), "Int16", -1)]
+        [InlineData(typeof(int), "Int32", -1)]
+        [InlineData(typeof(long), "Int64", -1)]
+        [InlineData(typeof(sbyte), "SByte", 2)]
+        [InlineData(typeof(string), "String", "abcd")]
+        [InlineData(typeof(ushort), "UInt16", 1)]
+        [InlineData(typeof(uint), "UInt32", 1)]
+        [InlineData(typeof(ulong), "UInt64", 1)]
+        public void GetJsonValue(Type type, string propertyName, object expValue)
+        {
+            var o = new
+            {
+                bo = true,
+                by = 1,
+                DateTime = default(DateTime),
+                DateTimeOffset = default(DateTimeOffset),
+                Decimal = 123,
+                Double = 123,
+                Guid = "72e791db-5223-4e06-9caf-39fd4b593de4",
+                Int16 = -1,
+                Int32 = -1,
+                Int64 = -1,
+                SByte = 2,
+                String = "abcd",
+                UInt16 = 1,
+                UInt32 = 1,
+                UInt64 = 1,
+            };
+            var jsonstring = JsonSerializer.Serialize(o);
+            using var jDoc = JsonDocument.Parse(jsonstring);
+            var root = jDoc.RootElement;
+            root.GetValue(type, propertyName).ShouldBe(expValue);
         }
 
         public class TestClass
--- src/AnyService.Utilities/Extensions/JsonExtensions.cs
+++ src/AnyService.Utilities/Extensions/JsonExtensions.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace System.Text.Json
 {
     public static class JsonExtensions
@@ -6,10 +9,38 @@ namespace System.Text.Json
         {
             return StringExtensions.ToObject<T>(jsonElement.ToString());
         }
-
         public static object ToObject(this JsonElement jsonElement, Type toType)
         {
             return StringExtensions.ToObject(jsonElement.ToString(), toType);
+        }
+
+        private static readonly IDictionary<Type, Func<JsonElement, object>> JsonValueConvert = new Dictionary<Type, Func<JsonElement, object>>
+        {
+            {typeof(bool), je => je.GetBoolean()},
+            {typeof(byte), je => je.GetByte()},
+            {typeof(DateTime), je => je.GetDateTime()},
+            {typeof(DateTimeOffset), je => je.GetDateTimeOffset()},
+            {typeof(decimal), je => je.GetDecimal()},
+            {typeof(double), je => je.GetDouble()},
+            {typeof(Guid), je => je.GetGuid()},
+            {typeof(short), je => je.GetInt16()},
+            {typeof(int), je => je.GetInt32()},
+            {typeof(long), je => je.GetInt64()},
+            {typeof(sbyte), je => je.GetSByte()},
+            {typeof(string), je => je.GetString()},
+            {typeof(ushort), je => je.GetUInt16()},
+            {typeof(uint), je => je.GetUInt32()},
+            {typeof(ulong), je => je.GetUInt64()},
+        };
+        public static T GetValue<T>(this JsonElement jsonElement, string propertyName)
+        {
+            return (T)GetValue(jsonElement, typeof(T), propertyName);
+        }
+        public static object GetValue(this JsonElement jsonElement, Type type, string propertyName)
+        {
+            if (!jsonElement.TryGetProperty(propertyName, out JsonElement value))
+                return default;
+            return JsonValueConvert[type](value);
         }
     }
 }
