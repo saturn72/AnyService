fefad6c520383e666409f626fcb7c7e5ffc54405
roi <shabtai>
2020-06-04 23:04:32 +0300

    add GuidStringGenerator

---
+++ src/AnyServiceModules/EntityFramework/AnyService.EntityFramework.Tests/ValueGenerators/GuidStringGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using AnyService.EntityFramework.ValueGenerators;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace AnyService.EntityFramework.Tests.ValueGenerators
+{
+    public class GuidStringGeneratorTests
+    {
+        [Fact]
+        public void GeneratesTemporaryValues_ReturnsFalse()
+        {
+            new GuidStringGenerator().GeneratesTemporaryValues.ShouldBeFalse();
+        }
+    }
+}
---
+++ src/AnyServiceModules/EntityFramework/AnyService.EntityFramework/ValueGenerators/GuidStringGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace AnyService.EntityFramework.ValueGenerators
+{
+    public class GuidStringGenerator : ValueGenerator<string>
+    {
+        private readonly SequentialGuidValueGenerator _guidGenerator = new SequentialGuidValueGenerator();
+        public override string Next(EntityEntry entry) => _guidGenerator.Next(entry).ToString();
+        public override bool GeneratesTemporaryValues => false;
+    }
+}
