c8d42583bd99bbb6618581c87a702df2a664acc5
roi <shabtai>
2020-06-21 14:13:32 +0300

    update mapping extensions

--- src/AnyService/MappingExtensions.cs
+++ src/AnyService/MappingExtensions.cs
@@ -23,7 +23,7 @@ namespace AnyService
         private static void AnyServiceMappingConfiguration(IMapperConfigurationExpression cfg)
         {
             cfg.CreateMap(typeof(Pagination<>), typeof(PaginationModel<>))
-                    .ForMember(nameof(PaginationModel<object>.Query), opts => opts.MapFrom(nameof(Pagination<IDomainModelBase>.QueryOrFilter)));
+                    .ForMember(nameof(PaginationModel<IDomainModelBase>.Query), opts => opts.MapFrom(nameof(Pagination<IDomainModelBase>.QueryOrFilter)));
         }
 
         public static IMapper MapperInstance => _mapper ??= mc.CreateMapper();
