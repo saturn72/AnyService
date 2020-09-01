22b72c52fc2a8229416e50b19269b9bcd34d7d85
roi <shabtai>
2020-09-01 12:54:03 +0300

    add extension methods for IEnumerable<EntityConfigRecord>

---
+++ src/AnyService.Tests/EntityConfigRecordExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper.Configuration.Annotations;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AnyService.Tests
+{
+    public class EntityConfigRecordExtensionsTests
+    {
+        readonly IEnumerable<EntityConfigRecord> _records = new[]
+            {
+                new EntityConfigRecord
+                {
+                    Name = "name-1",
+                    Type = typeof(string),
+                },new EntityConfigRecord
+                {
+                    Name = "name-2",
+                    Type = typeof(string),
+                }
+            };
+        [Fact]
+        public void FirstOrDefault_ReturnsDefault()
+        {
+            _records.FirstOrDefault(typeof(int)).ShouldBeNull();            
+        }
+        [Fact]
+        public void FirstOrDefault_ReturnsInstance()
+        {
+            _records.FirstOrDefault(typeof(string)).Name.ShouldBe("name-1");
+        }
+
+        [Fact]
+        public void First_Throws()
+        {
+            Should.Throw<InvalidOperationException>(() => _records.First(typeof(int)));
+        }
+        [Fact]
+        public void First_ReturnsInstance()
+        {
+            _records.First(typeof(string)).Name.ShouldBe("name-1");
+        }
+        [Fact]
+        public void All_returnsEmptyArray()
+        {
+            _records.All(typeof(int)).Count().ShouldBe(0);
+        }
+        [Fact]
+        public void All_returnsAll()
+        {
+            var a = _records.All(typeof(string ));
+            a.Count().ShouldBe(2);
+            a.ElementAt(0).Name.ShouldBe("name-1");
+            a.ElementAt(1).Name.ShouldBe("name-2");
+        }
+    }
+}
---
+++ src/AnyService/EntityConfigRecordExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnyService
+{
+    public static class EntityConfigRecordExtensions
+    {
+        /// <summary>
+        /// Gets EntityConfigRecord By Type
+        /// </summary>
+        /// <param name="records">All records</param>
+        /// <param name="type">Types to search</param>
+        /// <returns>matched EntityConfigRecord} or default</returns>
+        public static EntityConfigRecord FirstOrDefault(this IEnumerable<EntityConfigRecord> records, Type type) => records.FirstOrDefault(r => r.Type == type);
+
+
+        /// <summary>
+        /// Gets EntityConfigRecord By Type. throws if not match any entity
+        /// </summary>
+        /// <param name="records">All records</param>
+        /// <param name="type">Types to search</param>
+        /// <returns>matched EntityConfigRecord} or default</returns>
+        public static EntityConfigRecord First(this IEnumerable<EntityConfigRecord> records, Type type) => records.First(r => r.Type == type);
+
+        /// <summary>
+        /// Gets all instances matches specific type
+        /// </summary>
+        /// <param name="records">All records</param>
+        /// <param name="type">Types to search</param>
+        /// <returns>IEnumerable of EntityConfigRecords</returns>
+        public static IEnumerable<EntityConfigRecord> All(this IEnumerable<EntityConfigRecord> records, Type type) => records.Where(r => r.Type == type);
+    }
+}
